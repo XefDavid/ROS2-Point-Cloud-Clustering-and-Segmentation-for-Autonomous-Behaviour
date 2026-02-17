0dbe3381021c6e169d231b1605f44a48e583a08c
David <chefdavid@hotmail.com>
2026-02-17 12:25:37 +0100

    add: filter in pointcloud

--- point_cloud_processing/src/3_filtering_segmentation.cpp
+++ point_cloud_processing/src/3_filtering_segmentation.cpp
@@ -1,36 +1,58 @@
 #include <iostream>
-//Tipos de puntos
+// Tipos de puntos
 #include <pcl/point_types.h>
-//Lee y guarda archivos
+// Lee y guarda archivos
 #include <pcl/io/pcd_io.h>
-//Reduce puntos
+// Reduce puntos
 #include <pcl/filters/voxel_grid.h>
+//Reduce puntos
+#include <pcl/filters/passthrough.h>
 
 typedef pcl::PointXYZRGB PointT;
 
 int main()
 {
-    //creamos el punto compàrtido de datos especificos de pointT
-    pcl::PointCloud<PointT>::Ptr cloud (new pcl::PointCloud<PointT>);
-    pcl::PointCloud<PointT>::Ptr voxel_cloud (new pcl::PointCloud<PointT>);
+    // creamos el punto compàrtido de datos especificos de pointT
+    pcl::PointCloud<PointT>::Ptr cloud(new pcl::PointCloud<PointT>);
+    pcl::PointCloud<PointT>::Ptr voxel_cloud(new pcl::PointCloud<PointT>);
+    pcl::PointCloud<PointT>::Ptr passthrough_cloud(new pcl::PointCloud<PointT>); 
     pcl::PCDReader cloud_reader;
     pcl::PCDWriter cloud_writer;
 
-    //Reading the cloud
+    // Reading the cloud
     std::string path = "/home/david/ros2_ws/src/ROS2-Point-Cloud-Clustering-and-Segmentation-for-Autonomous-Behaviour/point_cloud_processing/point_clouds/";
     std::string cloud_name = "tb3_world.pcd";
-    std::string output_name = "voxel_cloud.pcd";
-    cloud_reader.read(path+cloud_name , *cloud);
+    //Aquí es donde se guarda con le nombre que queremos la nube elegida.
+    std::string output_name = "pass_xy_cloud.pcd";
+    cloud_reader.read(path + cloud_name, *cloud);
 
-    //voxel grid
-    pcl::VoxelGrid<PointT>voxel_filter;
+    // voxel grid
+    pcl::VoxelGrid<PointT> voxel_filter;
     voxel_filter.setInputCloud(cloud);
     // Estos valores son los que van a hacer la grilla más grande o mas pequeña
-    voxel_filter.setLeafSize(0.05 , 0.05, 0.05);
+    voxel_filter.setLeafSize(0.2, 0.2, 0.2);
     voxel_filter.filter(*voxel_cloud);
+
+    // Pass through Filter x
+    pcl::PassThrough<PointT> passing_x;
+    passing_x.setInputCloud(voxel_cloud);
+    passing_x.setFilterFieldName("x");
+    //el primer valor es la izquierda y el sugundo la derecha
+    passing_x.setFilterLimits(-1.5,1.5);
+    passing_x.filter(*passthrough_cloud);
+
+    //passthrough Filter Y
+    pcl::PassThrough<PointT> passing_y;
+    passing_x.setInputCloud(passthrough_cloud);
+    passing_x.setFilterFieldName("y");
+    //el primer valor es la izquierda y el sugundo la derecha
+    passing_x.setFilterLimits(-1.5,1.5);
+    passing_x.filter(*passthrough_cloud);
+
     
-    //clouds writing
-    cloud_writer.write<PointT>(path + output_name,*voxel_cloud,false);
+
+    // clouds writing
+    cloud_writer.write<PointT>(path + output_name, *passthrough_cloud, false);
 
     return 0;
 }
